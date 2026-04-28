e19158636f8fcd5e7c26a2dcd0b9ffe14588e510
Nanda Kishore <con3128@anarock.com>
2026-04-28 11:13:37 +0530

    second commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@ const App = () => {
   }
 
   return (
-    <Router>
+    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <Routes>
         {/* Public route: Login/Signup */}
         <Route
